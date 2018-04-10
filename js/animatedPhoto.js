a6f4712fb57b4a2e610b4cdfc89e9d7ead8e27b2
Pawel Wasil <wasil.paul@gmail.com>
2018-04-10 16:55:29 +0200

    animated photos srcipt add

---
+++ js/animatedPhoto.js
@@ -0,0 +1,16 @@
+$(document).ready(function ({
+
+                                $('#authors').hide();
+/*
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 1000) {
+        $('#authors').hide();
+        $('#authors').show();
+    } else {
+        $('#authors').show();
+        $('#authors').hide();
+    }
+});*/
+})
+;
