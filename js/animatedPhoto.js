2f542d17f3d0e2f5261bf664c177f9433845edb4
Pawel Wasil <wasil.paul@gmail.com>
2018-04-11 20:40:43 +0200

    WTF?

--- js/animatedPhoto.js
+++ js/animatedPhoto.js
@@ -1,16 +1,4 @@
-$(document).ready(function ({
 
-                                $('#authors').hide();
-/*
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 1000) {
-        $('#authors').hide();
-        $('#authors').show();
-    } else {
-        $('#authors').show();
-        $('#authors').hide();
-    }
-});*/
-})
-;
+inView('.rounded-circle')
+    .on('enter', $('.rounded-circle').hidden())
+});
---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var buttonNav = document.querySelector('.nav-entry')
+var classes = document.querySelector('#features')
+
+buttonNav.addEventListener('click', function() {classes.remove()})
+
+/*$(document).ready(function(){
+    $('.nav-entry').on('click', function{
+        $('#features').remove();
+    });
+});*/
