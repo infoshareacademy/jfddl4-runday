c1b4411c5119363e013b4d2cb6c3b1624534913f
Pawel Wasil <wasil.paul@gmail.com>
2018-04-16 23:50:09 +0200

    player move added

--- js/runDay.js
+++ js/runDay.js
@@ -3,11 +3,15 @@
     var _gameContainer = document.body
     var _gameBoard = null
     var _player = null
+    var _placePlayerX = 45
+    var _placePlayerY = 90
+
 
 
     function init() {
         createBoard()
         createPlayer()
+        attacListeners()
     }
 
     function createBoard() {
@@ -25,17 +29,17 @@
 
     function createPlayer() {
         var player = document.createElement('div')
-        player.style.position ='absolute'
-        player.style.width= '10%'
+        player.style.position = 'absolute'
+        player.style.width = '10%'
         player.style.height = '10%'
         player.style.backgroundColor = 'black'
         _gameBoard.appendChild(player)
 
         _player = player
-        placePlayer(45, 90)
+        placePlayer(_placePlayerX, _placePlayerY)
     }
 
-    function placePlayer(x, y){
+    function placePlayer(x, y) {
         _player.style.top = y + '%'
         _player.style.left = x + '%'
     }
@@ -44,9 +48,39 @@
 
     }
 
-    function attachEventListeners() {
+    function attacListeners() {
+        window.addEventListener('keydown', press, true)
 
+        function press(event) {
+            switch (event.keyCode) {
+                case 37:
+                    console.log('left')
+                    _placePlayerX -= 5
+                    if (_placePlayerX<0)
+                        _placePlayerX += 5
+                    console.log(_placePlayerX)
+                    createPlayer()
+                    break
+                case 39:
+                    _placePlayerX += 5
+                    if (_placePlayerX>90)
+                        _placePlayerX -= 5
+                    console.log(_placePlayerX)
+                    createPlayer()
+                    console.log("right")
+                    break
+                case 38:
+                    console.log('up')
+                    break
+                case 40:
+                    console.log("down")
+                    break
+            }
+
+        }
     }
+
+
 
     function move() {
 
