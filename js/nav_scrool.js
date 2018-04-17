938a0a2143af8610ea1bfeeebfb69b8afccad232
natalia struk <natalia.struk92@gmail.com>
2018-04-17 16:17:02 +0200

    add shrink to navbar

--- js/nav_scrool.js
+++ js/nav_scrool.js
@@ -36,3 +36,11 @@ $(document).ready(function() {
 
 
 });
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+    } else {
+        $('nav').removeClass('shrink');
+    }
+});
