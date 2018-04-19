7508181775f23bb680ff0f51202038e70711046f
Pawel Wasil <wasil.paul@gmail.com>
2018-04-19 16:42:37 +0200

    mmm

--- js/runDay.js
+++ js/runDay.js
@@ -43,6 +43,7 @@
         player.style.width = '10%'
         player.style.height = '10%'
         player.style.backgroundColor = 'black'
+        player.classList.add('player')
         _gameBoard.appendChild(player)
 
         _player = player
@@ -62,6 +63,7 @@
         fallingElement.style.height = '10%'
         fallingElement.style.backgroundColor = 'red'
         fallingElement.style.transition = 'all 2s ease-in'
+        fallingElement.classList.add('element')
         _gameBoard.appendChild(fallingElement)
         _fallingElement = fallingElement
 
@@ -96,18 +98,20 @@
     }
 
     function checkSmashEvent() {
+        var eachElement = document.querySelectorAll('.element')
+
+
         var playerLeft = parseFloat(_player.style.left)
         var elementLeft = parseFloat(_element.style.left)
         var playerTop = parseFloat(_player.style.top)
         var elementTop = parseFloat(_element.style.top)
 
-        if (elementLeft >= playerLeft && elementLeft < playerLeft + 10 && elementTop + 10 > playerTop
-            ||
-            elementLeft + 10 > playerLeft && elementLeft + 10 <= playerLeft + 10 && elementTop + 10 > playerTop
-        ) {
-            console.warn('crash', playerTop, elementTop + 10)
+        if (elementLeft - playerLeft <10 && playerLeft - elementLeft<40 && elementTop + 10 > playerTop)
+            {
+            console.warn('crash')
+
         } else {
-            console.log('nothing happened', playerTop, elementTop + 10)
+
         }
 
     }
@@ -144,68 +148,70 @@
                     break
             }
 
-        }}
-
-        function move() {
-            placePlayer(_placePlayerX, _placePlayerY)
         }
+    }
 
-        function gameTick() {
+    function move() {
+        placePlayer(_placePlayerX, _placePlayerY)
+    }
 
+    function gameTick() {
+
+    }
+
+
+    function incScore() {
+
+    }
+
+    function displayScore() {
+
+    }
+
+    var getInterval = setInterval(reduceTime, 1000)
+
+
+    function decTime(timeparam) {
+        var timeContainer = document.createElement('div')
+        _timeContainer = timeContainer
+        timeContainer.classList.add('time')
+        _gameBoard.appendChild(timeContainer)
+        timeContainer.style.position = 'absolute'
+        timeContainer.style.right = '10%'
+        timeContainer.style.top = '10%'
+        timeContainer.style.fontSize = '40px'
+        timeContainer.style.color = 'white'
+
+    }
+
+    function displayTIme() {
+    }
+
+    function reduceTime() {
+        _time++
+        _timeContainer.innerText = 'Your time: ' + _time;
+        if (_time === 0) {
+            clearInterval(getInterval)
         }
+    }
 
 
-        function incScore() {
-
-        }
-
-        function displayScore() {
-
-        }
-
-        var getInterval = setInterval(reduceTime, 1000)
+    function endGame() {
+    }
 
 
-        function decTime(timeparam) {
-            var timeContainer = document.createElement('div')
-            _timeContainer = timeContainer
-            timeContainer.classList.add('time')
-            _gameBoard.appendChild(timeContainer)
-            timeContainer.style.position = 'absolute'
-            timeContainer.style.right = '10%'
-            timeContainer.style.top = '10%'
-            timeContainer.style.fontSize = '40px'
-            timeContainer.style.color = 'white'
+    /*checking element must delete when game will be done*/
+    function createEl() {
+        var element = document.createElement('div')
+        element.style.position = 'absolute'
+        element.style.width = '40%'
+        element.style.height = '10%'
+        element.style.backgroundColor = 'red'
+        element.style.top = '81%'
+        element.style.left = '30%'
+        _gameBoard.appendChild(element)
+        _element = element
+    }
 
-        }
-
-        function displayTIme() {}
-
-            function reduceTime() {
-                _time++
-                _timeContainer.innerText = 'Your time: ' + _time;
-                if (_time === 0) {
-                    clearInterval(getInterval)
-                }
-            }
-
-
-        function endGame() {
-        }
-
-
-        /*checking element must delete when game will be done*/
-        function createEl() {
-            var element = document.createElement('div')
-            element.style.position = 'absolute'
-            element.style.width = '10%'
-            element.style.height = '10%'
-            element.style.backgroundColor = 'red'
-            element.style.top = '81%'
-            element.style.left = '30%'
-            _gameBoard.appendChild(element)
-            _element = element
-        }
-
-        init()
-    } )()
+    init()
+})()
